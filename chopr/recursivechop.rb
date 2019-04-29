501a89493990d410982f1a584390943847e3530d
Aviv Goll <6596595+avivg@users.noreply.github.com>
2019-04-29 09:56:30 +0300

    Ruby - Recursive implementation

--- chopr/recursivechop.rb
+++ chopr/recursivechop.rb
@@ -1,6 +1,25 @@
 module RecursiveChop
-  def chop(item, list)
-    idx = list.index(item)
-    idx.nil? ? -1 : idx
+  
+  def rec_chop(item, list, start, stop)
+    if start > stop
+      return -1
+    end
+    middle = ((start + stop)/2).to_int
+    middle_item = list[middle]
+    if middle_item == item
+      return middle
+    end
+    if middle_item < item
+      rec_chop(item, list, middle + 1, stop)
+    else
+      rec_chop(item, list, start, middle - 1)
+    end
   end
+
+  def chop(item, list)
+    start = 0
+    stop = list.size - 1
+    rec_chop(item, list, start, stop)
+  end
+
 end
